fee94dac86c7a78811447532eaf7893a709168c1
naknak987 <naknak987@gmail.com>
2018-10-22 06:59:50 -0400

    added auto hide selector

--- src/js/elementrequired.js
+++ src/js/elementrequired.js
@@ -2,16 +2,26 @@
  * These functions are required for certain form elements to work correctly.
  */
 // Select Box
-function trigger(list) {
+function trigger(list, ev) {
+    ev.stopPropagation();
     document.getElementById(list).classList.toggle('show-me');
 }
 
 function selected(selected, button, inputElId, items) {
     document.getElementById(button).innerHTML = selected.innerHTML + '<span class="chevron bottom"></span>';
     document.getElementById(inputElId).value = selected.innerHTML;
-    trigger(items);
+    //trigger(items);
+    document.getElementById(items).classList.toggle('show-me');
 }
 
+$(document).click(function() {
+    let all = document.getElementsByClassName('selector');
+    for(let i = 0; i < all.length; i++) {
+        if (all[i].classList.contains('show-me')) {
+            all[i].classList.toggle('show-me');
+        }
+    }
+})
 
 $(function () {
     $('#datepicker').datetimepicker({
